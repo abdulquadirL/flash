640aeafb0dae1854153308bd23ff527bab3203f8
buharry <elanquad92@gmail.com>
2024-03-20 11:53:38 +0100

    Updated changes

--- insta-plog/src/components/AuthForm/Sidebar/Sidebar.jsx
+++ insta-plog/src/components/AuthForm/Sidebar/Sidebar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Box, Flex, Link } from "@chakra-ui/react"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <Box 
+    height={"100px"}
+    borderRight={"1px solid"}
+    borderColor={"whiteAlpha.100"}
+    py={5}
+    position={"sticky"}
+    top={0}
+    left={0}
+    px={{base:2, md:4}}
+    >
+        <Flex>
+            <Link> </Link>
+        </Flex>
+
+    </Box>
   )
 }
 
--- insta-plog/src/pages/HomePage.jsx
+++ insta-plog/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import { Box, Text } from '@chakra-ui/react';
 
+const store = [{name: 'Next'}, {product: 'Nexter'}, {quantity: 5}]
+
 const HomePage = () => {
+  
   return (
     <Box>
      <Text>Hello Home</Text>
+     <Text> {store.name} </Text>
     </Box>
   )
 }
